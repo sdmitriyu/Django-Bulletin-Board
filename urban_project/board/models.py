29b2c4578f01b6667a9836288bd67f6d2bac1e9f
Дмитрий Соловьёв <sdmitriyyu@gmail.com>
2025-02-04 09:19:45 +0300

    Сохранение количества созданных объявлений (и, возможно, лайков)

--- urban_project/board/models.py
+++ urban_project/board/models.py
@@ -7,11 +7,15 @@ class Advertisement(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to='media/', blank=True, null=True, default='default.jpg')
-    likes = models.IntegerField(default=0)
-    dislikes = models.IntegerField(default=0)
+
+    def total_likes(self):
+        return self.likes.count()
+
+    def total_dislikes(self):
+        return self.dislikes.count()
 
     def __str__(self):
-        return self.title
+        return f"{self.title} - Likes: {self.like_set.count()}, Dislikes: {self.dislike_set.count()}"
 
 class Comment(models.Model):
     advertisement = models.ForeignKey(Advertisement, related_name='comments', on_delete=models.CASCADE)
@@ -22,15 +26,24 @@ class Comment(models.Model):
     def __str__(self):
         return f'Comment by {self.author} on {self.advertisement}'
 
-
 class Like(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Пользователь, который поставил лайк
-    advertisement = models.ForeignKey(Advertisement, on_delete=models.CASCADE)  # Объявление, которое получило лайк
-    created_at = models.DateTimeField(auto_now_add=True)  # Время, когда был поставлен лайк
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    advertisement = models.ForeignKey(Advertisement, related_name='likes', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f'Like by {self.user} on {self.advertisement}'
 
 
 class Dislike(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Пользователь, который поставил лайк
-    advertisement = models.ForeignKey(Advertisement, on_delete=models.CASCADE)  # Объявление, которое получило лайк
-    created_at = models.DateTimeField(auto_now_add=True)  # Время, когда был поставлен лайк
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    advertisement = models.ForeignKey(Advertisement, related_name='dislikes', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Dislike by {self.user} on {self.advertisement}'
+
+
+
+
+
--- urban_project/board/views.py
+++ urban_project/board/views.py
@@ -8,6 +8,11 @@ from django.contrib.auth import logout
 from .forms import SignUpForm
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
 
 def logout_view(request):
     logout(request)
@@ -119,65 +124,238 @@ def delete_advertisement(request, ad_id):
     return render(request, 'board/delete_advertisement.html', {'advertisement':advertisement})
 
 
+from django.shortcuts import get_object_or_404, redirect, render
+from .models import Advertisement, Like, Dislike
+
+
 def like_def(request, advertisement_id):
+    """
+    Считает отдельно количество лайков от зарегистрированных пользователей и от гостей
+    :param request:
+    :param advertisement_id:
+    :return:
+    """
     advertisement = get_object_or_404(Advertisement, id=advertisement_id)
 
     # Для аутентифицированных пользователей
     if request.user.is_authenticated:
         # Проверяем, ставил ли пользователь лайк
         if Like.objects.filter(user=request.user, advertisement=advertisement).exists():
-            # Если лайк уже существует, ничего не делаем и перенаправляем
-            return redirect("board:advertisement_detail", pk=advertisement.id)
+            # Если лайк уже существует, ничего не делаем
+            return render(
+                request,
+                'board/advertisement_detail.html',
+                {'advertisement': advertisement, 'total_likes': advertisement.total_likes()}
+            )
 
+        # Удаляем дизлайк, если он существует от этого пользователя
+        Dislike.objects.filter(user=request.user, advertisement=advertisement).delete()
         # Если лайка нет, создаём новый
         Like.objects.create(user=request.user, advertisement=advertisement)
 
     # Для гостей
     else:
         user_has_liked = request.session.get(f'has_liked_{advertisement_id}', False)
+        user_has_disliked = request.session.get(f'has_disliked_{advertisement_id}', False)
 
         if user_has_liked:
-            # Если лайк уже проставлен, просто перенаправляем
-            return redirect("board:advertisement_detail", pk=advertisement.id)
+            # Если лайк уже проставлен, ничего не делаем
+            return render(
+                request,
+                'board/advertisement_detail.html',
+                {'advertisement': advertisement, 'total_likes': advertisement.total_likes()}
+            )
+
+        # Заменяем дизлайк на лайк
+        if user_has_disliked:
+            request.session[f'has_disliked_{advertisement_id}'] = False
 
         # Помечаем, что лайк был поставлен
         request.session[f'has_liked_{advertisement_id}'] = True
 
-    # Увеличиваем количество лайков
-    advertisement.likes += 1
-    advertisement.save()
+    advertisement.refresh_from_db()  # Обновляем данные объявления
+    return render(
+        request,
+        'board/advertisement_detail.html',
+        {'advertisement': advertisement, 'total_likes': advertisement.total_likes()}
+    )
 
-    # Перенаправляем
-    return redirect("board:advertisement_detail", pk=advertisement.id)
 
 
 def dislike_def(request, advertisement_id):
-    advertisement = get_object_or_404(Advertisement, id=advertisement_id)
+    """
+    Считает количество дизлайков отдельно от зарегистрированных пользователей, и отдельно от гостей
+    :param request:
+    :param advertisement_id:
+    :return:
+    """
+    advertisement = get_object_or_404(Advertisement.total_dislikes(), id=advertisement_id)
 
     # Для аутентифицированных пользователей
     if request.user.is_authenticated:
-        # Проверяем, ставил ли пользователь лайк
-        if Dislike.objects.filter(user=request.user, advertisement=advertisement).exists():
-            # Если лайк уже существует, ничего не делаем и перенаправляем
-            return redirect("board:advertisement_detail", pk=advertisement.id)
+        # Проверяем, ставил ли пользователь дизлайк
+        if Dislike.objects.filter(user=request.user, advertisement=advertisement.total_dislikes()).exists():
+            # Если дизлайк уже существует, ничего не делаем
+            return render(
+                request,
+                'board/advertisement_detail.html',
+                {'advertisement': advertisement, 'total_dislikes': advertisement.total_dislikes()}
+            )
 
-        # Если лайка нет, создаём новый
-        Dislike.objects.create(user=request.user, advertisement=advertisement)
+        # Удаляем лайк, если он существует от этого пользователя
+        Like.objects.filter(user=request.user, advertisement=advertisement.total_likes()).delete()
+        # Если дизлайка нет, создаём новый
+        Dislike.objects.create(user=request.user, advertisement=advertisement.total_dislikes())
 
     # Для гостей
     else:
         user_has_disliked = request.session.get(f'has_disliked_{advertisement_id}', False)
+        user_has_liked = request.session.get(f'has_liked_{advertisement_id}', False)
 
         if user_has_disliked:
-            # Если лайк уже проставлен, просто перенаправляем
-            return redirect("board:advertisement_detail", pk=advertisement.id)
+            # Если дизлайк уже проставлен, ничего не делаем
+            return render(
+                request,
+                'board/advertisement_detail.html',
+                {'advertisement': advertisement, 'total_dislikes': advertisement.total_dislikes()}
+            )
 
-        # Помечаем, что лайк был поставлен
+        # Заменяем лайк на дизлайк
+        if user_has_liked:
+            request.session[f'has_liked_{advertisement_id}'] = False
+
+        # Помечаем, что дизлайк был поставлен
         request.session[f'has_disliked_{advertisement_id}'] = True
 
-    # Увеличиваем количество лайков
-    advertisement.dislikes += 1
+    advertisement.refresh_from_db()  # Обновляем данные объявления
+    return render(
+        request,
+        'board/advertisement_detail.html',
+        {'advertisement': advertisement, 'total_dislikes': advertisement.total_dislikes()}
+    )
+
+
+@receiver(post_save, sender=Advertisement)
+def update_ad_count_on_create(sender, instance, created, **kwargs):
+    """
+    Увеличивает счётчик оъявлений у связанного профильного пользователя
+    :param sender:
+    :param instance:
+    :param created:
+    :param kwargs:
+    :return:
+    """
+    if created:
+        profile = instance.user.profile
+        profile.ad_count += 1
+        profile.save()
+
+@receiver(post_delete, sender=Advertisement)
+def update_ad_count_on_delete(sender, instance, **kwargs):
+    """
+    Уеньшает счётчик оъявлений у связанного профильного пользователя, если счётчик больше нуля
+    :param sender:
+    :param instance:
+    :param kwargs:
+    :return:
+    """
+    profile = instance.user.profile
+    if profile.ad_count > 0:
+        profile.ad_count -= 1
+        profile.save()
+
+@receiver(post_save, sender=Advertisement)
+def update_like_dislike_count(sender, instance, created, **kwargs):
+    """
+    Увеличивает количество лайков или дизлайков в профиле пользователя и в объявлении.
+    :param sender:
+    :param instance:
+    :param created:
+    :param kwargs:
+    :return:
+    """
+    if created:
+        advertisement = instance.advertisement
+        profile = advertisement.user.profile
+        if instance.is_like:
+            profile.like_count += 1
+            advertisement.total_likes += 1
+        else:
+            profile.dislike_count += 1
+            advertisement.total_dislikes += 1
+        profile.save()
+        advertisement.save()
+
+@receiver(post_delete, sender=Advertisement)
+def update_like_dislike_count_on_delete(sender, instance, **kwargs):
+    """
+     Уменьшает количество лайков или дизлайков в профиле пользователя и в объявлении, если они больше нуля.
+    :param sender:
+    :param instance:
+    :param kwargs:
+    :return:
+    """
+    advertisement = instance.advertisement
+    profile = advertisement.user.profile
+    if instance.is_like and profile.like_count > 0:
+        profile.like_count -= 1
+        advertisement.total_likes -= 1
+    elif not instance.is_like and profile.dislike_count > 0:
+        profile.dislike_count -= 1
+        advertisement.total_dislikes -= 1
+    profile.save()
     advertisement.save()
 
-    # Перенаправляем
-    return redirect("board:advertisement_detail", pk=advertisement.id)
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    """
+    Если пользователь только что создан, функция создает связанный объект профиль.
+    :param sender:
+    :param instance:
+    :param created:
+    :param kwargs:
+    :return:
+    """
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@csrf_exempt
+@require_POST
+def like_advertisement(request, advertisement_id):
+    """
+     Используя `get_or_create`, добавляет лайк к указанному объявлению от текущего пользователя.
+     Удаляет дизлайк, если он ранее существовал.
+    :param request:
+    :param advertisement_id:
+    :return:
+    """
+    advertisement = get_object_or_404(Advertisement, id=advertisement_id)
+    user = request.user
+    Like.objects.get_or_create(user=user, advertisement=advertisement)
+    Dislike.objects.filter(user=user, advertisement=advertisement).delete()  # Удаляем дизлайк, если он был
+    response = {
+        'likes_count': advertisement.total_likes(),
+        'dislikes_count': advertisement.total_dislikes()
+    }
+    return JsonResponse(response)
+
+@csrf_exempt
+@require_POST
+def dislike_advertisement(request, advertisement_id):
+    """
+    Используя `get_or_create`, добавляет дизлайк к указанному объявлению от текущего пользователя.
+    Удаляет лайк, если он ранее существовал.
+    :param request:
+    :param advertisement_id:
+    :return:
+    """
+    advertisement = get_object_or_404(Advertisement, id=advertisement_id)
+    user = request.user
+    Dislike.objects.get_or_create(user=user, advertisement=advertisement)
+    Like.objects.filter(user=user, advertisement=advertisement).delete()  # Удаляем лайк, если он был
+    response = {
+        'likes_count': advertisement.total_likes(),
+        'dislikes_count': advertisement.total_dislikes()
+    }
+    return JsonResponse(response)
